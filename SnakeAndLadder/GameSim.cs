eb958da03dc848fbeee400b0253191a43d547aa7
AnshumanMunmun <imanshuman47@gmail.com>
2022-08-25 00:16:46 +0530

    [Anshuman] Add: Repeat till the Player reaches the winning

--- SnakeAndLadder/GameSim.cs
+++ SnakeAndLadder/GameSim.cs
@@ -19,21 +19,29 @@ namespace SnakeAndLadder
         }
         public void Play()
         {
-            int option = random.Next(0, 3);
-            switch (option)
+            while (playerPosition < 100)
             {
-                case NO_PLAY:
-                    playerPosition += 0;
-                    Console.WriteLine("The Player Stay's in the same position");
-                    break;
-                case LADDER:
-                    playerPosition += RollingDie();
-                    Console.WriteLine("The Player move's forward");
-                    break;
-                case SNAKE:
-                    playerPosition -= RollingDie();
-                    Console.WriteLine("The Player move's backward");
-                    break;
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case NO_PLAY:
+                        playerPosition += 0;
+                        Console.WriteLine("The Player Stay's in the same position");
+                        break;
+                    case LADDER:
+                        playerPosition += RollingDie();
+                        Console.WriteLine("The Player move's forward");
+                        break;
+                    case SNAKE:
+                        playerPosition -= RollingDie();
+                        Console.WriteLine("The Player move's backward");
+                        break;
+                        if (playerPosition < 0)
+                        {
+                            playerPosition = 0;
+                        }
+                        break;
+                }
             }
             Console.WriteLine("Rolling Die : " + RollingDie());
             Console.WriteLine("Player Position : " + playerPosition);
